d32220748d175e655663480c3d211cdc3aaf078f
sarahoco014 <soconnor2@outlook.com>
2023-07-10 14:40:42 +0100

    answering question 2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -16,9 +16,12 @@ public class Runner {
 
 //        1. Add "Coll" to the end of the list
 
-        scottishIslands.add("Coll");    
+        scottishIslands.add("Coll");
 
 //        2. Add "Tiree" to the start of the list
+
+        scottishIslands.add(0, "Tiree");
+
 //        3. Add "Islay" after "Jura" and before "Mull"
 //        4. Print out the index position of "Skye"
 //        5. Remove "Tresco" from the list by name
